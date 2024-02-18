982011e7db3c9eed38f1b6c22a2bfba14c83977d
Jxk0be <jakeshoffner27@gmail.com>
2024-02-18 01:44:24 -0500

    sizing + logo link

--- src/lib/components/Hero.svelte
+++ src/lib/components/Hero.svelte
@@ -3,14 +3,14 @@
 </script>
 
 <div class="h-screen w-full bg-slate-900 flex flex-col justify-center items-center">
-    <h1 class="font-bold text-5xl">
+    <h1 class="font-bold md:text-[65px] text-5xl">
         Ved.jx
     </h1>
-    <h1 class="font-bold text-5xl pb-3">
+    <h1 class="font-bold md:text-[65px] text-5xl pb-3">
         Digital Solutions
     </h1>
 
-    <div class="flex text-[20px] font-semibold text-[#50d89b]">
+    <div class="flex text-[20px] md:text-[30px] font-semibold text-[#50d89b]">
         &nbsp;
         <Typewriter mode="loopOnce" cursor={false} interval={[66, 76, 74, 70, 63, 54, 63, 64, 74, 64]}>
             <p class="uppercase">Custom solutions...</p>
--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -4,20 +4,23 @@
     let menuOpen = false;
 </script>
 
-<div class="w-full centered text-white z-30 flex justify-between items-center bg-[#212121] h-[90px] fixed max-w-[1500px] pl-5 pr-2 mx-auto">
-    <h1 class="font-bold text-2xl text-[#50d89b]">Ved.jx</h1>
-    <ul class="lg:flex hidden text-xl font-semibold">
-        <a href="/" on:click={() => menuOpen = false}><li class="px-3">Home</li></a>
-        <a href="services" on:click={() => menuOpen = false}><li class="px-3">Services</li></a>
-        <a href="about" on:click={() => menuOpen = false}><li class="px-3">About Us</li></a>
-        <a href="contact" on:click={() => menuOpen = false}><li class="px-3">Contact</li></a>
-    </ul>
-
-    <!-- Hamburger menu -->
-    <button class={`lg:hidden text-[35px] p-3 transition-all ease-in-out rounded-xl duration-150 ${menuOpen ? 'bg-[#121212]' : 'bg-[#212121]'}`} on:click={() => menuOpen = !menuOpen}>
-        <Icon icon="lucide:menu" />
-    </button>
+<div class="w-screen bg-[#212121] fixed h-[90px] z-30">
+    <div class="w-full centered text-white flex justify-between items-center h-[90px] fixed max-w-[1500px] pl-5 pr-2 mx-auto">
+        <a href="/" on:click={() => menuOpen = false}><h1 class="font-bold text-2xl text-[#50d89b]">Ved.jx</h1></a>
+        <ul class="lg:flex hidden text-xl font-semibold">
+            <a href="/" on:click={() => menuOpen = false}><li class="px-3">Home</li></a>
+            <a href="services" on:click={() => menuOpen = false}><li class="px-3">Services</li></a>
+            <a href="about" on:click={() => menuOpen = false}><li class="px-3">About Us</li></a>
+            <a href="contact" on:click={() => menuOpen = false}><li class="px-3">Contact</li></a>
+        </ul>
+    
+        <!-- Hamburger menu -->
+        <button class={`lg:hidden text-[35px] p-3 transition-all ease-in-out rounded-xl duration-150 ${menuOpen ? 'bg-[#121212]' : 'bg-[#212121]'}`} on:click={() => menuOpen = !menuOpen}>
+            <Icon icon="lucide:menu" />
+        </button>
+    </div>
 </div>
+
 
 <div class={`transition-all z-20 uppercase bg-[#0b0b0b] font-semibold min-w-[250px] text-white lg:hidden ease-in-out duration-150 ${menuOpen ? 'right-0' : 'md:right-[-40%] right-[-250px]'} fixed pt-[90px] w-[250px] top-0 md:w-[40%] h-full border-l border-gray-600`}>
     <ul>
