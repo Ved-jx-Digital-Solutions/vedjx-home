2d9275baf316236f6e93b6c7dd3db45df0c1493a
Jxk0be <JakeShoffner27@gmail.com>
2024-02-18 15:51:59 -0500

    mobile sizing change

--- src/lib/components/Hero.svelte
+++ src/lib/components/Hero.svelte
@@ -3,19 +3,19 @@
 </script>
 
 <div class="h-screen w-full flex flex-col justify-center items-center">
-    <h1 class="font-bold md:text-[65px] text-5xl">
+    <h1 class="font-bold md:text-[65px] mb-[-20px] text-[40px]">
         Ved.jx
     </h1>
-    <h1 class="font-bold md:text-[65px] text-5xl pb-3">
+    <h1 class="font-bold md:text-[65px] text-[40px] pb-3">
         Digital Solutions
     </h1>
 
-    <div class="flex text-[20px] uppercase md:text-[30px] font-semibold ">
+    <div class="flex text-[17px] uppercase md:text-[25px] font-semibold ">
         We provide&nbsp;
         <Typewriter mode="loop" cursor={false} interval={[66, 76, 74, 70, 63, 54, 63, 64, 74, 64]}>
             <p class="text-[#50d89b]">Custom solutions...</p>
-            <p class="text-[#50d89b]">concise user interfaces...</p>
-            <p class="text-[#50d89b]">a digital identity</p>
+            <p class="text-[#50d89b]">free consultations...</p>
+            <p class="text-[#50d89b]">a digital identity...</p>
         </Typewriter>
     </div>
 
