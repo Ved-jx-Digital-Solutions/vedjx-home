df5ae9a6390c553853a17ae78c56ed17a36c5a98
Jxk0be <jakeshoffner27@gmail.com>
2024-02-27 10:39:06 -0500

    small change

--- src/lib/components/Hero.svelte
+++ src/lib/components/Hero.svelte
@@ -18,19 +18,20 @@
 <div class="md:h-[40vh] bg-[#042b34] text-nowrap text-center h-screen w-full flex flex-col justify-center items-center">
     {#if transitionReady}
         <div transition:fly={{ x: -100, duration: 1000 }} class="w-full h-full flex justify-center items-center flex-col">
-            <h1 class="font-bold md:text-[65px] mb-[-15px] text-[40px] w-[300px]">
+            <h1 class="font-bold mb-[-15px] w-[250px] md:w-[275px]">
                 {@html Logo}
             </h1>
-            <h1 class="font-bold md:text-[65px] text-[40px] pb-3">
-                Digital Solutions
+            <h1 class="font-bold md:text-[65px] text-[45px]  pb-3">
+                Web Solutions
             </h1>
     
-            <div class="flex text-[17px] uppercase md:text-[25px] font-semibold ">
+            <div class="flex text-[18px] uppercase md:text-[25px] font-semibold min-w-[300px] justify-center">
                 We provide&nbsp;
                 <Typewriter mode="loop" cursor={false} interval={[66, 76, 74, 70, 63, 54, 63, 64, 74, 64]}>
                     <p class="text-[#38c5fd]">Custom solutions...</p>
-                    <p class="text-[#38c5fd]">free consultations...</p>
+                    <p class="text-[#38c5fd]">free quotes...</p>
                     <p class="text-[#38c5fd]">a digital identity...</p>
+                    <p class="text-[#38c5fd]">full stack development...</p>
                 </Typewriter>
             </div>
         </div>
